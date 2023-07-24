b8d896138410a8d1847fe9244a38bbd6a245df0c
Dohaibrahim106 <doha.ibrahim106@gmail.com>
2023-07-23 17:56:45 -0700

    printf

--- ZD_functions.c
+++ ZD_functions.c
@@ -1,47 +1,47 @@
 #include "main.h"
 /**
- * _zd_putchar - function prints character.
+ * _my_putchar - function prints character.
  * @c: parameter is character
  * Return: len
  */
-int _zd_putchar(char c)
+int _my_putchar(char c)
 {
 return (write(1, &c, 1));
 }
 /**
- * _zd_putstr - print string.
+ * _my_putstr - print string.
  * @s: parameter is string
  * Return: len.
  */
-int _zd_putstr(char *s)
+int _my_putstr(char *s)
 {
 return (write(1, s, strlen(s)));
 }
 /**
- * _zd_putint - print integer
+ * _my_putint - print integer
  * @n: number to be printed
  * return : return void
  */
-void _zd_putint(int n)
+void _my_putint(int n)
 {
 unsigned int n1;
 if (n < 0)
 {
 n1 = -n;
-_zd_putchar('-');
+_my_putchar('-');
 }
 else
 n1 = n;
 if (n1 / 10)
-_zd_putint(n1 / 10);
-_zd_putchar((n1 % 10) + '0');
+_my_putint(n1 / 10);
+_my_putchar((n1 % 10) + '0');
 }
 /**
- * _length_num - return the number len
+ * _num_length - return the number len
  * @n: number to be printed
  * Return: len.
  */
-int _length_num(int n)
+int _num_length(int n)
 {
 int len = 1;
 if (n < 0)
@@ -51,40 +51,41 @@ len++;
 return (len);
 }
 /**
- * _zd_check - print argument
+ * _check - print argument
  * @c: the string format
  * @args: the variable arguments
  * Return: len.
  */
-int _zd_check(char c, va_list args)
+int _check(char c, va_list args)
 {
 int num, len = 0;
 char *str;
 switch (c)
 {
 		case 'c':
-			len += _zd_putchar(va_arg(args, int));
+			len += _my_putchar(va_arg(args, int));
 			break;
 		case 's':
 			str = va_arg(args, char *);
-			len += _zd_putstr(STRING);
+			len += _my_putstr(STRING);
 			break;
 		case 'd':
 		case 'i':
 			num = va_arg(args, int);
-			_zd_putint(num);
-			len += _length_num(num);
+			_my_putint(num);
+			len += _num_length(num);
 			break;
 		case 'b':
-			len += _zd_printbin(va_arg(args, unsigned int));
+			len += _myprint_binary(va_arg(args, unsigned int));
 			break;
 		case '%':
-			len += _zd_putchar('%');
+			len += _my_putchar('%');
 			break;
 		default:
-			len += _zd_putchar('%');
-			len += _zd_putchar(c);
+			len += _my_putchar('%');
+			len += _my_putchar(c);
 			break;
 	}
 	return (len);
+}
 }
--- _ZD_printbin.c
+++ _ZD_printbin.c
@@ -1,103 +1,29 @@
 #include "main.h"
-
-unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-		unsigned char flags, int wid, int prec);
-unsigned int convert_ubase(buffer_t *output,
-		unsigned long int num, char *base,
-		unsigned char flags, int wid, int prec);
-
 /**
- * convert_sbase - Converts a signed long to an inputted base and stores
- *                 the result to a buffer contained in a struct.
- * @output: A buffer_t struct containing a character array.
- * @num: A signed long to be converted.
- * @base: A pointer to a string containing the base to convert to.
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: A precision modifier.
- *
- * Return: The number of bytes stored to the buffer.
+ * _myprint_binary - print a binary number
+ * @n: parameter is integer
+ * Return: len.
  */
-unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-		unsigned char flags, int wid, int prec)
+int _myprint_binary(unsigned int n)
 {
-	int size;
-	char digit, pad = '0';
-	unsigned int ret = 1;
-
-	for (size = 0; *(base + size);)
-		size++;
-
-	if (num >= size || num <= -size)
-		ret += convert_sbase(output, num / size, base,
-				flags, wid - 1, prec - 1);
-
-	else
-	{
-		for (; prec > 1; prec--, wid--) /* Handle precision */
-			ret += _memcpy(output, &pad, 1);
-
-		if (NEG_FLAG == 0) /* Handle width */
-		{
-			pad = (ZERO_FLAG == 1) ? '0' : ' ';
-			for (; wid > 1; wid--)
-				ret += _memcpy(output, &pad, 1);
-		}
-	}
-
-	digit = base[(num < 0 ? -1 : 1) * (num % size)];
-	_memcpy(output, &digit, 1);
-
-	return (ret);
+unsigned int m, i;
+unsigned int a[32];
+int len =  0;
+m = 2147483648;
+a[0] = n / m;
+for (i = 1; i < 32; i++)
+{
+m /= 2;
+a[i] = (n / m) % 2;
 }
-
-/**
- * convert_ubase - Converts an unsigned long to an inputted base and
- *                 stores the result to a buffer contained in a struct.
- * @output: A buffer_t struct containing a character array.
- * @num: An unsigned long to be converted.
- * @base: A pointer to a string containing the base to convert to.
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: A precision modifier.
- *
- * Return: The number of bytes stored to the buffer.
- */
-unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
-		unsigned char flags, int wid, int prec)
+for (i = 0; i < 31 ; i++)
+if (a[i])
+break;
+for (; i < 32; i++)
 {
-	unsigned int size, ret = 1;
-	char digit, pad = '0', *lead = "0x";
-
-	for (size = 0; *(base + size);)
-		size++;
-
-	if (num >= size)
-		ret += convert_ubase(output, num / size, base,
-				flags, wid - 1, prec - 1);
-
-	else
-	{
-		if (((flags >> 5) & 1) == 1) /* Printing a ptr address */
-		{
-			wid -= 2;
-			prec -= 2;
-		}
-		for (; prec > 1; prec--, wid--) /* Handle precision */
-			ret += _memcpy(output, &pad, 1);
-
-		if (NEG_FLAG == 0) /* Handle width */
-		{
-			pad = (ZERO_FLAG == 1) ? '0' : ' ';
-			for (; wid > 1; wid--)
-				ret += _memcpy(output, &pad, 1);
-		}
-		if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
-			ret += _memcpy(output, lead, 2);
-	}
-
-	digit = base[(num % size)];
-	_memcpy(output, &digit, 1);
-
-	return (ret);
+char z = '0' + a[i];
+write(1, &z, 1);
+len++;
+}
+return (len);
 }

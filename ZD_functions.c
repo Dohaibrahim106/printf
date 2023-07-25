540c17a7929b5be8c17ccc2885ab82e58bb5a9d8
Dohaibrahim106 <doha.ibrahim106@gmail.com>
2023-07-24 17:44:57 -0700

    printf

--- ZD_functions.c
+++ ZD_functions.c
@@ -1,132 +1,90 @@
 #include "main.h"
-
 /**
- * print_char - prints character
- * @ap: argument pointer
- * @params: the parameters struct
- *
- * Return: number chars printed
+ * _my_putchar - function prints character.
+ * @c: parameter is character
+ * Return: len
  */
-int print_char(va_list ap, params_t *params)
+int _my_putchar(char c)
 {
-	char pad_char = ' ';
-	unsigned int pad = 1, sum = 0, ch = va_arg(ap, int);
-
-	if (params->minus_flag)
-		sum += _putchar(ch);
-	while (pad++ < params->width)
-		sum += _putchar(pad_char);
-	if (!params->minus_flag)
-		sum += _putchar(ch);
-	return (sum);
+return (write(1, &c, 1));
 }
-
 /**
- * print_int - prints integer
- * @ap: argument pointer
- * @params: the parameters struct
- *
- * Return: number chars printed
+ * _my_putstr - print string.
+ * @s: parameter is string
+ * Return: len.
  */
-int print_int(va_list ap, params_t *params)
+int _my_putstr(char *s)
 {
-	long l;
-
-	if (params->l_modifier)
-		l = va_arg(ap, long);
-	else if (params->h_modifier)
-		l = (short int)va_arg(ap, int);
-	else
-		l = (int)va_arg(ap, int);
-	return (print_number(convert(l, 10, 0, params), params));
+return (write(1, s, strlen(s)));
 }
-
 /**
- * print_string - prints string
- * @ap: argument pointer
- * @params: the parameters struct
- *
- * Return: number chars printed
+ * _my_putint - print integer
+ * @n: number to be printed
+ * return : return void
  */
-int print_string(va_list ap, params_t *params)
+void _my_putint(int n)
 {
-	char *str = va_arg(ap, char *), pad_char = ' ';
-	unsigned int pad = 0, sum = 0, i = 0, j;
-
-	(void)params;
-	switch ((int)(!str))
-		case 1:
-			str = NULL_STRING;
-
-	j = pad = _strlen(str);
-	if (params->precision < pad)
-		j = pad = params->precision;
-
-	if (params->minus_flag)
-	{
-		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
-				sum += _putchar(*str++);
-		else
-			sum += _puts(str);
+unsigned int n1;
+if (n < 0)
+{
+n1 = -n;
+_my_putchar('-');
+}
+else
+n1 = n;
+if (n1 / 10)
+_my_putint(n1 / 10);
+_my_putchar((n1 % 10) + '0');
+}
+/**
+ * _num_length - return the number len
+ * @n: number to be printed
+ * Return: len.
+ */
+int _num_length(int n)
+{
+int len = 1;
+if (n < 0)
+len++;
+while (n /= 10)
+len++;
+return (len);
+}
+/**
+ * _check - print argument
+ * @c: the string format
+ * @args: the variable arguments
+ * Return: len.
+ */
+int _check(char c, va_list args)
+{
+int num, len = 0;
+char *str;
+switch (c)
+{
+		case 'c':
+			len += _my_putchar(va_arg(args, int));
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			len += _my_putstr(STRING);
+			break;
+		case 'd':
+		case 'i':
+			num = va_arg(args, int);
+			_my_putint(num);
+			len += _num_length(num);
+			break;
+		case 'b':
+			len += _myprint_binary(va_arg(args, unsigned int));
+			break;
+		case '%':
+			len += _my_putchar('%');
+			break;
+		default:
+			len += _my_putchar('%');
+			len += _my_putchar(c);
+			break;
 	}
-	while (j++ < params->width)
-		sum += _putchar(pad_char);
-	if (!params->minus_flag)
-	{
-		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
-				sum += _putchar(*str++);
-		else
-			sum += _puts(str);
-	}
-	return (sum);
-}
-
-/**
- * print_percent - prints string
- * @ap: argument pointer
- * @params: the parameters struct
- *
- * Return: number chars printed
- */
-int print_percent(va_list ap, params_t *params)
-{
-	(void)ap;
-	(void)params;
-	return (_putchar('%'));
-}
-
-/**
- * print_S - custom format specifier
- * @ap: argument pointer
- * @params: the parameters struct
- *
- * Return: number chars printed
- */
-int print_S(va_list ap, params_t *params)
-{
-	char *str = va_arg(ap, char *);
-	char *hex;
-	int sum = 0;
-
-	if ((int)(!str))
-		return (_puts(NULL_STRING));
-	for (; *str; str++)
-	{
-		if ((*str > 0 && *str < 32) || *str >= 127)
-		{
-			sum += _putchar('\\');
-			sum += _putchar('x');
-			hex = convert(*str, 16, 0, params);
-			if (!hex[1])
-				sum += _putchar('0');
-			sum += _puts(hex);
-		}
-		else
-		{
-			sum += _putchar(*str);
-		}
-	}
-	return (sum);
+	return (len);
 }
--- _printf.c
+++ _printf.c
@@ -1,49 +1,27 @@
 #include "main.h"
-
 /**
- * _printf - prints anything
+ * _printf - function prints output according to a format
  * @format: the format string
- *
- * Return: number of bytes printed
+ * Return: the number of chars
  */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
-
-	va_start(ap, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
-	{
-		init_params(&params, ap);
-		if (*p != '%')
-		{
-			sum += _putchar(*p);
-			continue;
-		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
-		{
-			p++; /* next char */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-				params.l_modifier || params.h_modifier ? p - 1 : 0);
-		else
-			sum += get_print_func(p, ap, &params);
-	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+unsigned int i, len = 0;
+va_list args;
+if (format == NULL)
+return (-1);
+va_start(args, format);
+for (i = 0; format[i]; i++)
+{
+if (format[i] != '%')
+{
+len += _my_putchar(format[i]);
+continue;
+}
+if (i++ == strlen(format) - 1)
+return (-1);
+len += _check(format[i], args);
+}
+va_end(args);
+return (len);
 }
